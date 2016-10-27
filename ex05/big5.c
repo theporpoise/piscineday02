61bfd9de9961fdb2d0167f87bb9bfa302bab118f
Matthew Gould <matt@everettgould.com>
2016-10-27 15:18:05 -0700

    adding in day five, need to configure last solution

--- ex05/big5.c
+++ ex05/big5.c
@@ -6,7 +6,7 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/26 23:01:06 by mgould            #+#    #+#             */
-/*   Updated: 2016/10/27 08:26:22 by mgould           ###   ########.fr       */
+/*   Updated: 2016/10/27 15:17:18 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,41 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-//write a function to output the putchar - 6 lines
-//write a function to init the variables - 9 lines
+void	out_char(int i, int j, int k, int l)
+{
+	ft_putchar(('0' + i));
+	ft_putchar(('0' + j));
+	ft_putchar((' '));
+	ft_putchar(('0' + k));
+	ft_putchar(('0' + l));
+	if ((i == 9) && (j == 8))
+	{
+		return ;
+	}
+	ft_putchar((','));
+	ft_putchar((' '));
+}
+
+void	loop_count(int i, int j, int k, int l)
+{
+	while (++i <= 9)
+	{
+		while (++j <= 9)
+		{
+			while (++k <= 9)
+			{
+				while (++l <= 9)
+				{
+					out_char(i, j, k, l);
+				}
+				l = -1;
+			}
+			k = i - 1;
+			l = j + 1;
+		}
+		j = -1;
+	}
+}
 
 void	ft_print_comb2(void)
 {
@@ -32,33 +65,8 @@ void	ft_print_comb2(void)
 	i = -1;
 	j = -1;
 	k = -1;
-	l = -1;
-
-	while (++i <= 9)
-	{
-		while (++j <= 9)
-		{
-			while (++k <= 9)
-			{
-				while (++l <= 9)
-				{
-					ft_putchar(('0' + i));
-					ft_putchar(('0' + j));
-					ft_putchar(' ');
-					ft_putchar(('0' + k));
-					ft_putchar(('0' + l));
-					ft_putchar((','));
-					ft_putchar((' '));
-				}
-				l = -1;
-			}
-			k = -1; 
-			l = -1;
-		}
-		j = 0; 
-		k = 0;
-		l = -1;
-	}
+	l = 0;
+	loop_count(i, j, k, l);
 }
 
 int	main(void)
