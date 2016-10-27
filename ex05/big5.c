0e6d3a18f5d6bd3c4ea348e2abc916dcd3bd6ec4
Matthew Gould <matt@everettgould.com>
2016-10-27 14:16:45 -0700

    still working on exercise 5, looks like I had an error in my iteration.  fun to try solving it though

--- ex05/big5.c
+++ ex05/big5.c
@@ -6,7 +6,7 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/26 23:01:06 by mgould            #+#    #+#             */
-/*   Updated: 2016/10/27 12:58:51 by mgould           ###   ########.fr       */
+/*   Updated: 2016/10/27 08:26:22 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,42 +19,45 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	out_char(int i, int j, int k, int l)
-{
-	ft_putchar((47 + i));
-	ft_putchar((47 + j));
-	ft_putchar(' ');
-	ft_putchar((47 + k));
-	ft_putchar((47 + l));
-	if ((i + j + k + l) < 40)
-	{
-		ft_putchar((','));
-		ft_putchar((' '));
-	}
-}
+//write a function to output the putchar - 6 lines
+//write a function to init the variables - 9 lines
 
 void	ft_print_comb2(void)
 {
-	static int array[4];
+	int i;
+	int j;
+	int k;
+	int l;
 
-	while (++(array[0]) <= 10)
+	i = -1;
+	j = -1;
+	k = -1;
+	l = -1;
+
+	while (++i <= 9)
 	{
-		while (++(array[1]) <= 10)
+		while (++j <= 9)
 		{
-			while (++(array[2]) <= 10)
+			while (++k <= 9)
 			{
-				while (++(array[3]) <= 10)
+				while (++l <= 9)
 				{
-					out_char(array[0], array[1], array[2], array[3]);
+					ft_putchar(('0' + i));
+					ft_putchar(('0' + j));
+					ft_putchar(' ');
+					ft_putchar(('0' + k));
+					ft_putchar(('0' + l));
+					ft_putchar((','));
+					ft_putchar((' '));
 				}
-				array[3] = (array[2] + array[1] + array[0] - 3);
+				l = -1;
 			}
-			array[2] = (array[1] + array[0] - 2);
-			array[3] = (array[2] + array[1] +array[0] -3);
+			k = -1; 
+			l = -1;
 		}
-		array[1] = 0;
-		array[2] = (array[1] + array[0] - 2);
-		array[3] = (array[2] + array[1] +array[0] -3);
+		j = 0; 
+		k = 0;
+		l = -1;
 	}
 }
 
