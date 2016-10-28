d42ebe192e8c6dda961bddaecfb288fea1727065
Matthew Gould <matt@everettgould.com>
2016-10-27 19:07:03 -0700

    clearning up before submit and move to day 3

--- ex06/format.c
+++ ex06/format.c
@@ -1,36 +1,67 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   test6.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/10/26 15:01:44 by mgould            #+#    #+#             */
-/*   Updated: 2016/10/26 17:28:59 by mgould           ###   ########.fr       */
+/*   Created: 2016/10/27 15:38:53 by mgould            #+#    #+#             */
+/*   Updated: 2016/10/27 19:01:29 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	ft_print_alphabet(void)
+void	get_counts(int count, int count2, int comp)
 {
-	int i;
+	;
+}
 
+void	ft_putnbr(int nb)
+{
+	int count2;
+	int comp;
+	int count;
+	int i;
+	
 	i = 0;
-	while (('a' + i) <= 'z')
+	comp = nb;
+	count = 0;
+	count2 = 0;
+
+	get_counts(count, count2, comp);
+
+	while (comp > 10)
 	{
-		ft_putchar(('a' + i));
-		i = i + 1;
+		count++;
+		count2++;
+		comp = comp / 10;
+	}
+
+	int num_array[count];
+
+	while (count >= 0)
+	{
+		num_array[count] = (nb % 10);
+		count--;
+		nb = nb / 10;		
+	}
+
+	while (i <= count2)
+	{
+		ft_putchar('0' + num_array[i]);
+		i++;
 	}
 }
 
-int	main(void)
+int		main(void)
 {
-	ft_print_alphabet();
+	ft_putnbr(420);
 	return (0);
 }
