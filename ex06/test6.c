1ac811bd5b39de2cc9dfbd1e5949c1f60821ab3c
Matthew Gould <matt@everettgould.com>
2016-10-27 18:46:37 -0700

    got number six, yeah

--- ex06/test6.c
+++ ex06/test6.c
@@ -6,7 +6,7 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/27 15:38:53 by mgould            #+#    #+#             */
-/*   Updated: 2016/10/27 16:15:21 by mgould           ###   ########.fr       */
+/*   Updated: 2016/10/27 18:45:48 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,41 +18,48 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int		ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
+	printf("you got here\n");	
+	int count2;
 	int comp;
 	int count;
+	int i;
+	i = 0;
 	comp = nb;
-	int print;
+	count = 0;
+	count2 = 0;
 
-	while (nb > 10)
+	while (comp > 10)
 	{
+		printf("inside comp loop \n");
 		count++;
-		nb = nb / 10;
+		count2++;
+		comp = comp / 10;
 	}
 
-	while(count > -1)
-	{
-		//printf("nb is %d \n", nb);
-		print = comp - (count * 10);
-		ft_putchar((comp + '0'));
-		
-		while(comp > 10)
-		{
-			comp = comp / 10;
-			count++; 
-		}
-		ft_putchar((comp % 10) + '0');
-		nb = nb - (comp * count * 10);
-		printf(" comp times count %d", (comp * count * 10));
-		comp = nb;
+	int num_array[count];
 
-	}	
-	return (0);
+	while (count >= 0)
+	{
+		printf("inside array loop");
+		num_array[count] = (nb % 10);
+		printf(" count is %d and nb mod is %d", count, (nb % 10));
+		printf("numarray at count is %d\n", num_array[count]);
+		count--;
+		nb = nb / 10;		
+	}
+
+	while (i <= count2)
+	{
+		//printf("number i %d\n", num_array[i]);
+		ft_putchar('0' + num_array[i]);
+		i++;
+	}
 }
 
 int		main(void)
 {
-	ft_putnbr(42);
+	ft_putnbr(420);
 	return (0);
 }
